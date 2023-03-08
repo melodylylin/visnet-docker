43c26fc9320d9bf6d95c27eb3ce667dc59d67e03
zpyang <yang2zhanpeng@gmail.com>
2023-03-08 11:57:19 -0500

    build px4 dependencies and dds agents

--- install/px4_setup.sh
+++ install/px4_setup.sh
@@ -4,7 +4,7 @@ set -x
 
 wget https://raw.githubusercontent.com/PX4/PX4-Autopilot/main/Tools/setup/ubuntu.sh
 wget https://raw.githubusercontent.com/PX4/PX4-Autopilot/main/Tools/setup/requirements.txt
-bash ubuntu.sh --no-sim-tools
+bash ubuntu.sh --no-sim-tools && rm ubuntu
 
 git clone https://github.com/eProsima/Micro-XRCE-DDS-Agent.git
 cd Micro-XRCE-DDS-Agent
